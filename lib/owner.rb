32f2317a85d8b118e2a32c290af9ab1581c4fb3c
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 04:14:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,15 @@
 class Owner
   # code goes here
   attr_reader :name
+  
+  @@all = []
+  
   def initialize(name)
     @name = name
     @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
