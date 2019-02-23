a09ef865b433ad60694068d49829b096e6ea8b79
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 04:28:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -73,6 +73,6 @@ class Owner
   end
   
   def list_pets
-    return "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)"
+    return "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end
 end
